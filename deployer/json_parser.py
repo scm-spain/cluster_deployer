3c48bfd3fa66484f09aba42c478cef7b571b817a
David Pozos <david.pozos@infojobs.net>
2016-11-02 16:53:43 +0100

    try to receive status

--- deployer/json_parser.py
+++ deployer/json_parser.py
@@ -17,7 +17,7 @@ def decode_url_json(my_json):
 # It checks if the status is working progress, and return working,
 # Otherwise return the status of the stack. It only check the first entry on completedTaskList section.
 def check_stack_status(json_decoded, stack_name):
-    result = ""
+    result = None
     for iterator in json_decoded["runningTaskList"]:
         if iterator["objectId"] == stack_name:
             result = "working"
@@ -33,6 +33,6 @@ def check_stack_status(json_decoded, stack_name):
 def check_asgard_stack_status(asgard_url, stack):
     asgard_url += "/task/list.json"
     json_decoded = load_json_from_url(asgard_url)
-    print(check_stack_status(json_decoded, stack))
+    return check_stack_status(json_decoded, stack)
 
 #check_asgard_stack_status("http://ec2-52-18-32-197.eu-west-1.compute.amazonaws.com:8080", "ms_autocomplete-awscfdeployms3122-v000")
